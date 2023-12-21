203999e3e14dd8afae3e6d56ef5592712bb650ae
xabru <xabru@yandex.com>
2023-12-21 01:26:32 -0500

    chore: add bin value in package.json file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
+#!/usr/bin/env node
+
 import Cli from './cli';
 
-if (import.meta.url === `file://${process.argv[1]}` || !import.meta.url) {
-  Cli(...process.argv.slice(2));
-}
-
-export default Cli;
+Cli(...process.argv.slice(2));
